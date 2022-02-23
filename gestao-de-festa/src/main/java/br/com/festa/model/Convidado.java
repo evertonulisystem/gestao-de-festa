dc34c5a763e2abf75e3d20c7ffee6269c95dcc48
Everton Ulisses <evertonulisystem@gmail.com>
2022-02-23 14:16:01 -0300

    Enviando gitIgnore

--- gestao-de-festa/src/main/java/br/com/festa/model/Convidado.java
+++ gestao-de-festa/src/main/java/br/com/festa/model/Convidado.java
@@ -2,12 +2,9 @@ package br.com.festa.model;
 
 import java.io.Serializable;
 import java.util.Objects;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
